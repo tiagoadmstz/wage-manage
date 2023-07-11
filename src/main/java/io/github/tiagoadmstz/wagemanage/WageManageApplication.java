563c90ca06d961c8ed2a424d5d2ef0ed1840f72a
tiagoadmstz <tiagoadmstz@yahoo.com.br>
2023-07-11 20:36:53 -0300

    initial commit

---
+++ src/main/java/io/github/tiagoadmstz/wagemanage/WageManageApplication.java
@@ -0,0 +1,14 @@
+package io.github.tiagoadmstz.wagemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WageManageApplication.class, args);
+		String.
+	}
+
+}
---
+++ src/test/java/io/github/tiagoadmstz/wagemanage/WageManageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tiagoadmstz.wagemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WageManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
